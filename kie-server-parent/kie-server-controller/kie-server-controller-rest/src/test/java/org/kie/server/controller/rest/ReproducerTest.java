692faa162df28fda66e7c71f29ab2a25d93f62b4
Cristiano Nicolai <cristiano.nicolai@gmail.com>
2020-01-10 16:33:42 +1000

    Reproducer

--- kie-server-parent/kie-server-controller/kie-server-controller-rest/src/test/java/org/kie/server/controller/rest/ReproducerTest.java
+++ kie-server-parent/kie-server-controller/kie-server-controller-rest/src/test/java/org/kie/server/controller/rest/ReproducerTest.java
@@ -1,7 +1,6 @@
 package org.kie.server.controller.rest;
 
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
 
 import org.junit.Before;
@@ -11,50 +10,47 @@ import org.kie.server.controller.api.model.spec.ServerTemplate;
 import org.kie.server.controller.api.storage.KieServerTemplateStorage;
 import org.kie.server.controller.impl.storage.InMemoryKieServerTemplateStorage;
 
+import static org.junit.Assert.fail;
 import static org.kie.server.controller.rest.ControllerUtils.marshal;
 
 public class ReproducerTest {
 
-    private KieServerTemplateStorage templateStorage = InMemoryKieServerTemplateStorage.getInstance();
     private static final String serverTemplateId = "id";
-
+    private KieServerTemplateStorage templateStorage = InMemoryKieServerTemplateStorage.getInstance();
     private CountDownLatch serverUp = new CountDownLatch(1);
 
     @Before
     public void setUp() {
         ServerTemplate serverTemplate = new ServerTemplate(serverTemplateId, "name");
         IntStream.range(0, 1000)
-                 .boxed()
-                 .map(i -> Integer.toString(i))
-                 .map(s -> new ServerInstanceKey(s, s, s, ""))
-                 .forEach(instance -> serverTemplate.addServerInstance(instance));
+                .boxed()
+                .map(i -> Integer.toString(i))
+                .map(s -> new ServerInstanceKey(s, s, s, ""))
+                .forEach(instance -> serverTemplate.addServerInstance(instance));
         templateStorage.store(serverTemplate);
     }
 
     @Test
-    public void testConcurrency() throws InterruptedException {
+    public void testConcurrency() {
         final ServerTemplate serverTemplate = templateStorage.load(serverTemplateId);
+        marshal("application/xml", serverTemplate);
 
         Thread disconnect = new Thread(() -> disconnect());
         disconnect.start();
 
         // Sync before marshalling
-        serverUp.await(5, TimeUnit.SECONDS);
-        String response = marshal("application/xml", serverTemplate);
-        System.out.println(response);
+        marshal("application/xml", serverTemplate);
     }
 
     public void disconnect() {
         ServerTemplate serverTemplate = templateStorage.load(serverTemplateId);
 
         try {
-            serverUp.countDown();
             for (ServerInstanceKey instanceKey : serverTemplate.getServerInstanceKeys()) {
                 serverTemplate.deleteServerInstance(instanceKey.getServerInstanceId());
-                Thread.sleep(10);
             }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        } catch (Exception e) {
+            fail(e.getMessage());
         }
     }
 }
